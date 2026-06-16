99810409c7a448be7aa9a67e374dff6ea3a030d4
Jormaedes Luís <jormahed@gmail.com>
2026-06-16 16:57:33 +0100

    feat: add global store

---
+++ src/store/useCartStore.js
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+
+const useCartStore = create((set, get) => ({
+	items: [],
+
+	addItem: (product) => {
+		const items = get().items;
+		const existing = items.find((i) => i.id === product.id);
+
+		if (existing) {
+			set({
+				items: items.map((i) =>
+					i.id === product.id ? { ...i, quantity: i.quantity + product.quantity } : i
+				)
+			});
+		} else {
+			set({
+				items: [...items, { ...product, quantity:  product.quantity}]
+			})
+		}
+	},
+
+	removeItem: (id) => {
+		set((state) => ({
+			items: state.items.map((i) =>
+				(i.id === id ? { ...i, quantity } : i)
+			)
+		}))
+	},
+
+	clearCart: () => set({ items: [] }),
+
+	getTotalItems: () => get().items.reduce((acc, i) => acc + i.quantity, 0),
+
+	getTotalPrice: () => get().items.reduce((acc, i) => acc + i.price * i.quantity, 0)
+}));
+
+export default useCartStore;
