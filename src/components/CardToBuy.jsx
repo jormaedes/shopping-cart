b3d5bd2ff93cae1739bb418d6e9480778c4e4c46
Jormaedes Luís <jormahed@gmail.com>
2026-06-17 12:29:07 +0100

    feat: update cart
    - add funcionality to update quantity of items

--- src/components/CardToBuy.jsx
+++ src/components/CardToBuy.jsx
@@ -1,33 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useCartStore from "../store/useCartStore";
 const CardToBuy = ({ id, title, image, price, quantity }) => {
-	const { items, incrementProduct,removeItem, updateItem, clearCart, getTotalPrice } = useCartStore();
+	const { items, incrementProduct, removeItem, updateItem, clearCart, getTotalPrice } = useCartStore();
 	const [qtd, setQtd] = useState(quantity);
 
+	useEffect(() => {
+		incrementProduct(id, qtd);
+	}, [qtd])
+
 	return (
-		<div key={id} className='bg-gray-100 p-4 rounded-lg flex gap-4'>
+		<div className='bg-gray-100 p-4 rounded-lg flex gap-4'>
 			<img src={image} alt={title} className='w-24 h-24 object-cover rounded' />
 			<div className='flex-1'>
 				<h3 className='font-bold mb-2'>{title}</h3>
 				<p className='text-gray-600 mb-3'>${price}</p>
 				<div className='flex items-center gap-2'>
-					<button
-						onClick={() => updateItem(id, -1)}
+					<button disabled={qtd <= 1}
+						onClick={() => {
+							if (qtd <= 1) return;
+							setQtd(qtd - 1)
+						}
+						}
 						className='bg-gray-300 px-2 py-1 rounded'
 					>
 						-
 					</button>
 					<input
+						min={1}
 						type='number'
 						value={qtd}
 						onChange={(e) => {
+							if (e.target.value === '' || parseInt(e.target.value) <= 0) {
+								setQtd(1);
+								return;
+							}
 							setQtd(parseInt(e.target.value))
-							incrementProduct(id, qtd)
 						}}
 						className='w-12 [appearance:textfield] text-center border rounded'
 					/>
 					<button
-						onClick={() => { updateItem(id, 1) }}
+						onClick={() => { setQtd(qtd + 1) }}
 						className='bg-gray-300 px-2 py-1 rounded'
 					>
 						+
