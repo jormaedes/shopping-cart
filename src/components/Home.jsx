346a1dd01f117d6fe2eb8e50bcd74816a29044e2
Jormaedes Luís <jormahed@gmail.com>
2026-06-17 12:27:48 +0100

    feat: add bg-img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router";
 
 const Home = () => {
 	return (
-		<main className="min-h-[94vh] text-black flex justify-center items-center flex-col text-center gap-4">
-			<h2 className="text-3xl md:text-5xl font-bold">Welcome to <span>Online</span>
+		<main className="min-h-[94vh] bg-cover bg-[url(/bg.jpg)] text-black flex justify-center items-center flex-col text-center gap-4">
+			<h2 className="text-3xl md:text-5xl font-extrabold text-white">Welcome to <span>Online</span>
 				<span className='text-red-600'>Shop</span>
 			</h2>
-			<p>Your global grocery store — find anything and everything you need.</p>
+			<p className="text-white font-medium">Your global grocery store — find anything and everything you need.</p>
 			<div>
 				<Link className="text-white font-medium bg-red-600 py-2 px-4 rounded-md hover:bg-red-800" to='shop'>Start Shopping</Link>
 			</div>
